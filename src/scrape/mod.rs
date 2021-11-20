47282b0b9f058c48faaa1f76dcec3480a037c1b6
Chris Foster <cdbfoster@gmail.com>
2021-11-20 18:59:32 -0500

    Added a scrape for the miscellaneous info on a tournament page.

--- src/scrape/mod.rs
+++ src/scrape/mod.rs
@@ -1,6 +1,7 @@
 use crate::util::cache::{Cache, CacheMap, PageCache};
 
 use self::tournament_event_group_list::EventGroupList;
+use self::tournament_info::Info;
 use self::tournament_list::TournamentList;
 use self::tournament_player_list::PlayerList;
 use self::tournament_schedule::Schedule;
@@ -8,6 +9,7 @@ use self::tournament_schedule::Schedule;
 pub use self::result::{scrape_result, ScrapeResult};
 
 pub mod tournament_event_group_list;
+pub mod tournament_info;
 pub mod tournament_list;
 pub mod tournament_player_list;
 pub mod tournament_schedule;
@@ -19,6 +21,7 @@ pub struct ScrapeCache {
     pub pages: PageCache,
     pub tournament_list: Cache<TournamentList>,
     pub tournament_event_list: CacheMap<usize, EventGroupList>,
+    pub tournament_info: CacheMap<usize, Info>,
     pub tournament_player_list: CacheMap<usize, PlayerList>,
     pub tournament_schedule: CacheMap<usize, Schedule>,
 }
@@ -27,6 +30,7 @@ const TOURNAMENT_LIST_REFRESH: u64 = 60 * 60;
 const TOURNAMENT_EVENT_BRACKET_LIST_REFRESH: u64 = 2 * 60 * 60;
 const TOURNAMENT_EVENT_BRACKET_PAGE_REFRESH: u64 = 15 * 60;
 const TOURNAMENT_EVENT_PLAYER_LIST_REFRESH: u64 = 3 * 60 * 60;
+const TOURNAMENT_INFO_REFRESH: u64 = 3 * 60 * 60;
 const TOURNAMENT_PAGE_REFRESH: u64 = 60 * 60;
 const TOURNAMENT_PLAYER_LIST_REFRESH: u64 = 3 * 60 * 60;
 const TOURNAMENT_SCHEDULE_REFRESH: u64 = 2 * 60 * 60;
---
+++ src/scrape/tournament_info.rs
@@ -0,0 +1,83 @@
+use std::time::Duration;
+
+use regex::Regex;
+use scraper::{Html, Selector};
+
+use crate::client::Client;
+use crate::scrape::{ScrapeCache, ScrapeResult, TOURNAMENT_INFO_REFRESH, TOURNAMENT_PAGE_REFRESH};
+use crate::util::cache::{CacheGuard, CacheMapGuard};
+use crate::util::guard_stack::GuardStack;
+
+pub type Info = Vec<(String, String)>;
+
+pub type InfoGuard<'a> =
+    GuardStack<'a, (CacheMapGuard<'a, usize, Info>, CacheGuard<'a, Info>), Info>;
+
+pub async fn tournament_info<'a>(
+    tournament_id: usize,
+    client: &'a Client<'a>,
+    cache: &'a ScrapeCache,
+) -> ScrapeResult<InfoGuard<'a>> {
+    cache
+        .tournament_info
+        .get(tournament_id)
+        .await
+        .try_push_guard_async(|info_cache| async move {
+            info_cache
+                .retrieve_or_update(Duration::from_secs(TOURNAMENT_INFO_REFRESH), || async {
+                    let tournament_page_url = format!(
+                        "https://www.pickleballtournaments.com/tournamentinfo.pl?tid={}",
+                        tournament_id
+                    );
+
+                    let tournament_page_raw_html = cache
+                        .pages
+                        .retrieve_or_update(
+                            Duration::from_secs(TOURNAMENT_PAGE_REFRESH),
+                            &tournament_page_url,
+                            |url| async { client.get(url).send().await },
+                            "could not load tournament info",
+                        )
+                        .await?
+                        .clone();
+
+                    let tournament_page = Html::parse_document(&tournament_page_raw_html);
+
+                    let nav_item_selector =
+                        Selector::parse(".nav > .nav-item > .nav-link").unwrap();
+
+                    let href_pattern = Regex::new(r#"href="([^"]+)""#).unwrap();
+
+                    Ok(tournament_page
+                        .select(&nav_item_selector)
+                        .filter_map(|l| {
+                            let html = l.html();
+                            href_pattern
+                                .captures(&html)
+                                .map(|c| (c[1].to_owned(), l.inner_html()))
+                        })
+                        .filter(|(l, _)| l.starts_with("#"))
+                        .filter(|(l, _)| {
+                            [
+                                "#menuSchedule",
+                                "#menuPlayerList",
+                                "#menuEventList",
+                                "#menuPlayersNeedingPartners",
+                                "#menuFindPlayer",
+                            ]
+                            .iter()
+                            .all(|t| l != t)
+                        })
+                        .filter_map(|(l, n)| {
+                            let selector = Selector::parse(&l).unwrap();
+                            tournament_page
+                                .select(&selector)
+                                .next()
+                                .map(|e| (n, e.inner_html()))
+                        })
+                        .collect())
+                })
+                .await
+        })
+        .await
+}
